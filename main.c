2ad64a01f79b7ea616274ace56b3518ecbae5f10
MYPHUONG0609 <53520113+MYPHUONG0609@users.noreply.github.com>
2019-12-08 11:45:22 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define MAX 100
+#include "sort.h"
+void nhap (int a[], int n) {
+for (int i=0; i<=n;i++) {
+printf("\nNhap phan tu a[%d]:",i);
+scanf("%d",&a[i]);
+}
+}
+
+void xuat (int a[],int n) {
+    for (int i=0;i<=n;i++) {
+        printf("%4d",a[i]);
+    }
+    
+}
+int main() {
+    int n;
+    int a[MAX];
+   
+    printf("nhap n");
+    scanf("%d",&n);
+  printf("nhap mang");
+    nhap(a,n);
+    printf("xuat mang");
+    xuat(a,n);
+    int *bubble = bubblesort(a,n);
+   
+    printf("mang sap xep tang dan la %d",bubble);
+return 0;
+}
---
+++ sort.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int *bubblesort( register int a[],int n);
+int *selection(register int a[],int n);
+
+   
+
