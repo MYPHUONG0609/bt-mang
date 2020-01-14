7381f1525a3df4c79f5c890de3cf1d16b478f473
MYPHUONG0609 <53520113+MYPHUONG0609@users.noreply.github.com>
2020-01-14 19:25:18 +0700

    Add files via upload

--- mainofSINX.c
+++ mainofSINX.c
@@ -2,18 +2,18 @@
 #include<math.h>
 int Giaithua(float x, int n){
     if (n==0)
-    return 0;
-    return 2*n+Giaithua(x,n-1);
+    return 1;
+    return (2*n+1)+Giaithua(x,n-1);
 }
 float Tinh_mu(float x, int n){
     if (n==0)
-    return 1;
-    return pow(x,2*n)+Tinh_mu(x,n-1);
+    return x;
+    return pow(x,2*n+1)+Tinh_mu(x,n-1);
 }
-float Cosx(float x, int n){
+float Sinx(float x, int n){
     if (n==0)
-    return 1;
-    return (pow(-1,n)*(Tinh_mu(x,n)/Giaithua(x,n)))+Cosx(x,n-1);
+    return x;
+    return (pow(-1,n)*(Tinh_mu(x,n)/Giaithua(x,n)))+Sinx(x,n-1);
 }
 int main (){
     int n;
@@ -22,6 +22,6 @@ int main (){
     scanf("%d",&n);
     printf("Nhap x:");
     scanf("%f",&x);
-    printf("Cosx cua %f la %f",x,Cosx(x,n));
+    printf("Sinx cua %f la %f",x,Sinx(x,n));
     return 0;
 }
